a06aa9c5baade52ed5bc9431452dedcf5ae6f374
Prabha Sapkota <sapkotaprabha96@gmail.com>
2023-09-01 17:54:40 -0400

    Added three functions

--- user_math_prabha.py
+++ user_math_prabha.py
@@ -1,17 +1,8 @@
 """
 Author = Prabha Sapkota    
 Date = 08-31-2023
-Purpose: Illustrate the math module and how to write
-defensive functions by trying them, 
+Purpose: Attempting Task 4 to write defensive functions by using math module, 
 and if things don't work provide a custom message.
-
-Use try / except / finally whenever a valid function could cause an error
-e.g.,
-- a number is not valid such as a negative radius or age
-- a string is empty or missing
-- the requested resource could not be found
-
-@ uses math module - for some advanced math
 
 """
 
@@ -21,45 +12,45 @@ from util_logger import setup_logger
 logger, logname = setup_logger(__file__)
 
 
-def get_leaf_area(height, width):
+def get_field_area(height, width):
     """
-    Return area of a leaf given the height and width.
-
-    @param height: the height of the leaf, width: the width of leaf
-    @return: the area of the leaf
-    @raise Exception: if height, widhth is not a number
+    Return area of a rectangular experimental field where biological experiment is designed given the height and width.
+    Args:
+    @param height: the height of the field, width: the width of field
+    @return: the area of the field
+    @raise Exception: if height, width is not a number
 
     """
 
     
-    logger.info(f"CALLING get_leaf_area({height, width})")
+    logger.info(f"CALLING get_field_area({height, width})")
 
     try: 
         area = height * width 
-        logger.info(f"The leaf area is {area}cm*cm")
+        logger.info(f"The field area is {area}sq.m")
         return area
     except Exception as ex:
         logger.error(f"Error: {ex}")
         return None
     
-def get_leaf_areas_given_dimensions(height, width):
+def get_field_areas_given_dimensions(height, width):
     """
-    This function should return the area of a rectangle given its width and height
+    This function should return the area of a rectangle experimental field given its height and width.
 
     Keyword arguments:
-    width -- the width of the leaf
-    height -- the height of the leaf
+    width -- the width of the field
+    height -- the height of the field
     """
     # Implement the calculation logic here
     return width * height
 
 
-def get_leaf_areas_given_list(height_list, width_list):
+def get_field_areas_given_list(height_list, width_list):
     """
-    This function should return a list with the area of each leaf.
+    This function should return a list with the area of each experimental rectangular field.
     """
 
-    logger.info(f"CALLING get_leaf_areas_given_lists({height_list, width_list})")
+    logger.info(f"CALLING get_field_areas_given_lists({height_list, width_list})")
 
     if not height_list or not width_list:
         logger.error("Please add some items to the lists. Nothing to do.")
@@ -75,7 +66,7 @@ def get_leaf_areas_given_list(height_list, width_list):
     # for every pair of height and width values in the lists 
     for height, width in zip(height_list, width_list): 
         try:
-            area = get_leaf_areas_given_dimensions(height, width)
+            area = get_field_areas_given_dimensions(height, width)
             logger.info(f"height = {height}, width = {width}, area={area}")
             area_list.append(area)
 
@@ -84,16 +75,16 @@ def get_leaf_areas_given_list(height_list, width_list):
 
 
 
-def get_total_leafnumbers(leafnumbers_list):
+def get_total_plantsnumbers(plantnumbers_list):
     """
-    This function should return a list with leafnumbers in a plant.
+    This function should return a list with plantnumbers in a field.
     
     """
-    logger.info(f"CALLING get_total_leafnumbers({leafnumbers_list})") 
+    logger.info(f"CALLING get_total_plantnumbers({plantnumbers_list})") 
 
     try:
-         total = sum(leafnumbers_list)
-         logger.info(f"Total leafnumbers is {total}")
+         total = sum(plantnumbers_list)
+         logger.info(f"Total plantnumbers is {total}")
          return total
     except Exception as ex:
          logger.error(f"Error: {ex}")
@@ -119,20 +110,21 @@ if __name__ == "__main__":
     logger.info("")
 
     logger.info("TRY: Call get_leaf_area_given_height()_and_width() function with a different values.")
-    get_leaf_area(4,2)
-    get_leaf_area(6,5)
-    get_leaf_area(3, 2)
+    get_field_area(50,20)
+    get_field_area(64,51)
+    get_field_area(33, 21)
     logger.info("")
 
     logger.info("TRY: Call get_leaf_area_list() function with a list of values")
-    height_list = [9, 3, 6, 8, 4, 5]
-    width_list = [2, 1, 3, 2, 4, 3]
-    get_leaf_areas_given_list(height_list, width_list)
+    height_list = [39, 38, 60, 80, 45, 58]
+    width_list = [23, 19, 38, 22, 44, 35]
+    get_field_areas_given_list(height_list, width_list)
     logger.info("")
     
     logger.info("TRY: Call get_total_leafnumbers() function with a different values")
-    total_leafnumbers_list = [3, 4, 2, 10, 5]
-    get_total_leafnumbers(total_leafnumbers_list)
+    total_plantnumbers_list = [31, 87, 220, 102, 58]
+    get_total_plantsnumbers(total_plantnumbers_list)
+    logger.info("")
     
     print("Done. Please check the log file for more details.")
 
